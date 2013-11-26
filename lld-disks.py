08534a88df9668ef930760deda604b04aff036df
Vyacheslav Stetskevych <slava18@gmail.com>
2013-11-26 02:37:16 +0200

    Downdated to run on python 2.6

--- lld-disks.py
+++ lld-disks.py
@@ -3,8 +3,25 @@
 import json
 import subprocess
 
+## Downdated to run on python 2.6
+
+# Added function implementation
+def check_output(*popenargs, **kwargs):
+    process = subprocess.Popen(stdout=subprocess.PIPE, *popenargs, **kwargs)
+    output, unused_err = process.communicate()
+    retcode = process.poll()
+    if retcode:
+        cmd = kwargs.get("args")
+        if cmd is None:
+            cmd = popenargs[0]
+        error = subprocess.CalledProcessError(retcode, cmd)
+        error.output = output
+        raise error
+    return output
+
 if __name__ == '__main__':
-    output = subprocess.check_output("cat /proc/diskstats | awk '{print $3}' | grep -v 'ram\|loop\|sr'", shell=True)
+    #output = subprocess.check_output("cat /proc/diskstats | awk '{print $3}' | grep -v 'ram\|loop\|sr'", shell=True)
+    output = check_output("cat /proc/diskstats | awk '{print $3}' | grep -v 'ram\|loop\|sr'", shell=True)
     data = list()
     for line in output.split("\n"):
         if line:
